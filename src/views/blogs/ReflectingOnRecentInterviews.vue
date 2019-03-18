8ce17ded1871569f942d5c2f71bad6aa620a68eb
Phil Cross <p.cross@live.co.uk>
2019-03-18 16:25:01 +0000

    Add "Reflecting on Recent Interviews" blog

---
+++ src/views/blogs/ReflectingOnRecentInterviews.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <p>Recently, the tech team where I current work was unfortunately subject to redundancies. We're only a small team, and I know I was highly certain to be selected. It's never nice to lose your job, but at the same time, maybe it's a good time to start looking to see what else is out there.</p>
+        <p>Firstly, I'm mainly a backend developer. I'm fairly confident with Javascript and Vuejs, but my CSS knowledge is terrible. This was something I was actively working on prior to any announcements. When looking through job adverts, one of my personal biggest turn offs is seeing "PHP Developer required - good knowledge of CSS/SCSS essential".</p>
+        <p>I know enough CSS to get by, If given a pre-written template, or something from themeforest, I have a good enough working knowledge to make changes to make it look how I want. But what I struggle with most is all the edge cases. If the browser is this, then do X, otherwise do Y. If the browser is this but is lower than version 10, do Z, and so on.</p>
+
+        <p class="page-title">The initial search</p>
+        <p>With the current state of tech, there's also so much you need to know. You can't just get away with knowing a single language anymore. You need to know HTML, CSS, Javascript, Javascript frameworks and libraries (Vuejs / jQuery), frontend build processes, Npm, Yarn, ES6, Webpack, Babel, backend languages, backend frameworks, and then knowledge of CI/CD, AWS, Google Cloud, build pipelines etc.</p>
+        <p>There's so much stuff you need to know. At companies I've worked for, there's usually dedicated people, or teams that handle different areas of the stack. Frontend Developers, Backend developers, DevOps. I have working knowledge of what all these things are - I can put together a really basic deployment pipeline with github and digital ocean, but trying to understand everything is often to much.</p>
+        <p>And I think thats one of the problems with job adverts lately. Companies are so focussed on trying to get a developer who can do it all, rare as those might be, that often the developer with a basic working knowledge of the tech pass by. I saw a few adverts which I would have probably applied for, if there was less requirements to having expert knowledge of a whole stack.</p>
+        <p>That being said, I know what I need to learn. A lot of the jobs I passed by needed CSS. I'll work on that, and I have done with this site. I haven't build a site from scratch in a long time, so forcing myself to work with CSS (via Tailwind) felt like a good practical exersize.</p>
+        <p>And obviously my new site is going to deployed, so will hopefully revisit build pipelines.</p>
+
+        <p class="page-title">The application process</p>
+        <p>I applied for a few jobs. I've been a remote worker for 18 months now, and it really fits my lifestyle. Instead of commuting on a train for a few hours a day, I can now spend that time at home playing with my 3 year old. Remote work, while hard work, is really beneficial. Trying to find a remote, primarily backend PHP developer job, living in the south west of England (known more for Farming and countryside than its barely existant tech scene) is super hard.</p>
+        <p>I was really lucky finding the job I'm leaving. Trying to find a similar one was a like looking for a needle in a haystack. Often adverts will state "Remote working" as a benefit, but often it's only 1 or 2 days a week. You won't know until you apply.</p>
+        <p>The first job I applied for looked promising, and even got a reply from one of the directors saying the application has been forwarded to the head of product. Unfortunately, that was the end of the communication. Which was disappointing. I look hard to find jobs which I'm best suited at. For every 100 adverts, I might apply for 2.</p>
+        <p>That's not me being picky, that's just trying to find a job which will keep pushing me, but something I can do. So not hearing back from a company which I think I'd fit well into is unsettling.</p>
+        <p>I also applied for a job with company making booking software, which I did hear back from, but they didn't feel I'd be a good fit. Which is perfectly fine. Again, it's disheartening to hear, but they know best about what they want.</p>
+        <p>I did get interviews for 2 jobs, one in Bournemouth, and one in Bristol.</p>
+
+        <p class="page-title">The coding tests</p>
+        <p>Whats a tech interview without a coding test! I personally have quite enjoyed the coding tests I've had over the years. It gives me a chance to see how far I've come from the first coding test I ever did. I get to apply all the knowledge I've gained from previous positions, into a single purposeful feature.</p>
+        <p>Both tests I got to do at home, and was given a weekend for one, and a day for another. Both of which I pulled all-nighters on. I'm typically a night owl anyway. I have a really nice view from my front room window out to sea, and there's something about everything being quiet, at night, no cars, sea view, which some netflix series on in the background that really helps me concentrate.</p>
+        <p>I wouldn't say the tests were hard, one needed me to implement some filesystem logic, and the other required me to build up a complete dataset by querying 2 endpoints on a unreliable REST api. Both of the challenges are on my github. <a href="https://github.com/philcross/catstorage" target="_blank" class="text-blue hover:text-blue-darker no-underline">https://github.com/philcross/catstorage</a> and <a href="https://github.com/philcross/itccompliance" target="_blank" class="text-blue hover:text-blue-darker no-underline">https://github.com/philcross/itccompliance</a>.</p>
+        <p>I really enjoyed the test in the second link. One of the requirements was to build the application without any framework. I've worked with frameworks like Laravel for years now, they do all the hard work, so actually deciding the best way to approach the task, without the framework was quite fun.</p>
+
+        <p class="page-title">The interviews</p>
+        <p>The tests must have been pretty good, because I was invited for interviews for both of them. I'm an introvert, and like probably most developers have a degree of impostor syndrome. I try to push myself into giving demo's of project work when I can, but I still get pretty sweaty and shaky with nerves!</p>
+        <p>I really don't enjoy being interviewed. There's so much pressure, even more so when you don't have the security of if the interview doesn't go well, you can just go back to your current job. I've been pretty lucky throughout my career. Most of my interviews have gone pretty well, and I think most of that success is due to a high level or preparation.</p>
+        <p>A couple of days before the interview, I'll open up an editor, and jot down every possible thing I can think of they might ask. Why I'm looking for a new job? What are my strengths? What are my weaknesses? What do I know about the company? I try to do a lot of preparing for each interview, and thats been super beneficial. It let's me forget about questions I haven't prepared for. I'm not scrambling my brain for questions I know the answer to.</p>
+        <p>The Bristol interview, which was done via Skype, I thought went really well, and it must of done as I was offered the job. The bournemouth one I'm not too sure about. I had to drive to their offices, which were about 2.5 hours away (5 hours total drive). The offices were the most impressive offices I've seen. They were really awesome. The tech they were making was really awesome, and the company as a whole felt like it wouuld be a great company to work for.</p>
+        <p>While I did do a lot of preparation for the interview, I think the drive there might have had some kind of effect on me. More of the questions that I would have liked caught me out a little, not enough to not be able to answer them, but I found myself repeating a few anecdotes. That's not to say I don't think I did well, just I could have done a bit better if I was more relaxed.</p>
+        <p>Ultimately I accepted the Bristol job.</p>
+
+        <p class="page-title">What happens next</p>
+        <p>I've got 4 weeks before I start my new job. While the advert seemed to fit me perfectly, it was clear in the interview that I needed to know more about certain areas - specifically microservices and lurking more into the dev-ops realm. So, that's what I've been doing.</p>
+        <p>Interviews shouldn't just be about trying to get the job, they should hopefully show you areas of improvement. I'm both insanely nervous about starting the new job, but also very excited. For the first time, I'll be a senior developer, something I've wanted to do for a long time - not for the title, but I've always wanted to help more junior team members, and this opportunity is a great place to start.</p>
+        <p>Hopefully I'll be writing a few more blogs over the next 4 weeks as I build up knowledge for the new job.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ReflectingOnRecentInterviews',
+    }
+</script>
--- src/views/blogs/index.js
+++ src/views/blogs/index.js
@@ -1,9 +1,9 @@
 export default [
     {
-        id: 'bacon-ipsum-1',
-        title: 'This is a test 1',
+        id: 'reflecting-on-recent-interviews',
+        title: 'Reflecting on Recent Interviews',
         date: 'Sunday, 17th March 2019, 23:13',
-        preview: 'This is a test blog post, this is a test blog post',
-        file: 'BaconIpsum1',
+        preview: 'Lately, I\'ve been on the look out for a new job. It\'s been pretty stressful, but thought I should jot down my thoughts.',
+        file: 'ReflectingOnRecentInterviews',
     },
 ];
